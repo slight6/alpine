464e195cb858c11b8417528dadca6cc3b50b70fa
jason consiglio <jason@27pro.com>
2023-04-08 23:33:21 -0400

    changed format, not implemented yet

---
+++ script.js
@@ -0,0 +1,13 @@
+const totalTime = document.getElementById('#time');
+
+
+// need . input for max, or over the max, and then k would be the number * 1000, and m for 1000000
+
+new Cleave('.donation', {
+    numeral: true,
+    numeralThousandGroupStyle: 'thousand',
+    prefix: '$',
+
+})
+
+
